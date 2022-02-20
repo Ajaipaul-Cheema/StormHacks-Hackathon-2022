77c5121bc0b4a56155e1bd5258febfcffe66dd5c
Haris Ahmad <HarisAhmad04@hotmail.com>
2022-02-19 21:32:57 -0800

    Summarizer UI complete

--- client/src/components/Sharedtextarea.jsx
+++ client/src/components/Sharedtextarea.jsx
@@ -5,21 +5,25 @@ import './textarea.css';
 class Sharedtextarea extends Component {
   render() {
     return (
-      <div>
-        <label>Enter Text: </label>
-        <br></br>
-        <input id="mainbox" type="textare" 
-          name="Text"
-          onChange={this.props.handleChange}
-        />
-        <br></br>
-        <br></br>
-         <input id="linenumber" type="text" 
-          name="sentenceNumber"
-          onChange={this.props.handleChange}
-        />
-        <br></br>
-         <button type="submit">Summarize</button>
+      <div class="container">
+        <form >
+          <label>Enter Text To Summarize: </label><br />
+          <br />
+          
+          <textarea id="mainbox" type="textare"
+            name="Text"
+            onChange={this.props.handleChange}
+          />
+          <br />
+          <label>Sentence Number: </label><br /><br />
+          <input id="linenumber" type="number"
+            name="sentenceNumber"
+            onChange={this.props.handleChange}
+            oninput="validity.valid||(value='');" min="1"
+          />
+          <br></br><br />
+          <button type="submit">Summarize</button>
+        </form>
       </div>
 
     );
